0699098e2c4225cb561c035635e0f9690718a135
Damien Corticchiato <skaradams69@gmail.com>
2019-11-18 12:10:23 +0100

    add containers + css fun in menu

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,34 +4,37 @@ import profilePicture from "../assets/images/profile-picture.jpg"
 
 const Layout = ({ children }) => (
   <div className="column">
-    <div className="layout-header row">
-      <div className="profile-picture no-flex" >
-        <img src={ profilePicture } />
-      </div>
-      <div className="layout-navigation column">
-        <div className="layout-menu row">
-          <div className="no-flex active">
-            Présentation
+    <div className="layout-header">
+      <div className="container row">
+        <div className="profile-picture no-flex" >
+          <img src={ profilePicture } />
+        </div>
+        <div className="layout-navigation column">
+          <div className="layout-menu row">
+            <div className="no-flex active">
+              Présentation
+            </div>
+            <div className="no-flex">
+              Parcours
+            </div>
+            <div className="no-flex">
+              Réalisations
+            </div>
+            <div className="no-flex">
+              Contact
+            </div>
           </div>
-          <div className="no-flex">
-            Parcours
-          </div>
-          <div className="no-flex">
-            Réalisations
-          </div>
-          <div className="no-flex">
-            Contact
+          <div className="layout-info">
+            <h1>
+              Damien CORTICCHIATO
+            </h1>
+            <h2>
+              Full-stack developper
+            </h2>
           </div>
         </div>
-        <div className="layout-info">
-          <h1>
-            Damien CORTICCHIATO
-          </h1>
-          <h2>
-            Full-stack developper
-          </h2>
-        </div>
       </div>
+
     </div>
     <div className="container">
       { children }
