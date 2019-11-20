0d5dff36ec8f13f47ce72c0c9bf6a4d406dc89c3
Damien Corticchiato <skaradams69@gmail.com>
2019-11-20 15:40:28 +0100

    add icons for presentation page

--- src/components/StackRatio.js
+++ src/components/StackRatio.js
@@ -2,10 +2,8 @@ import React from 'react';
 
 const StackRatio = () => (
   <div className="stack-ratio row">
-    <span className="stack-bar front-bar" style={{width: "65%"}}>
-    </span>
-    <span className="stack-bar back-bar" style={{width: "35%"}}>
-    </span>
+    <span className="stack-bar front-bar" style={{width: "65%"}} />
+    <span className="stack-bar back-bar" style={{width: "35%"}} />
   </div>
 )
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,34 @@
 import React from 'react'
 import StackRatio from "../components/StackRatio";
-
+import timelineIcon from "../assets/images/icon-timeline.svg";
 const Home = () => (
   <div className="page page-home">
+    <div className="row">
+      <div className="presentation-info">
+        <div className="info-wrapper">
+          <div className="info-icon icon-experience"/>
+        </div>
+        <div className="info-text">
+          5 ans d'expérience en start-up
+        </div>
+      </div>
+      <div className="presentation-info">
+        <div className="info-wrapper">
+          <div className="info-icon icon-responsive"/>
+        </div>
+        <div className="info-text">
+          Forte sensibilité à l'UI/UX et design responsive
+        </div>
+      </div>
+      <div className="presentation-info">
+        <div className="info-wrapper">
+          <div className="info-icon icon-code"/>
+        </div>
+        <div className="info-text">
+          Respect des conventions pour un code propre et maintenable
+        </div>
+      </div>
+    </div>
     <StackRatio />
   </div>
 )
