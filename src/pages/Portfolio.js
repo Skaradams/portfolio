770b9794795b276b0c066e8d55f026f1d9af98f0
Damien Corticchiato <skaradams69@gmail.com>
2019-12-02 19:07:33 +0100

    add portfolio thumb

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,7 +1,17 @@
 import React from 'react'
+import portfolio from '../assets/images/portfolio/portfolio.png'
 
 const Portfolio = () => (
   <div className="page portfolio-page">
+    <div className="portfolio-list">
+      <div className="portfolio-slot">
+        <div className="portfolio-thumb" id="portfolio">
+        </div>
+        <div className="portfolio-title">
+          Portfolio
+        </div>
+      </div>
+    </div>
   </div>
 )
 
